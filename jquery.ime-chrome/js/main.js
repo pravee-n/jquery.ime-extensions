e2bbd9f78a24b5570c61c84da6dbb89804aa3993
Prageck <prageck@gmail.com>
2013-05-03 21:30:34 +0530

    Fixed issue with image loading

--- jquery.ime-chrome/js/main.js
+++ jquery.ime-chrome/js/main.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
-	var keyboardIcon = chrome.extension.getURL("/images/ime-active.png");
-	$( '.imeselector' ).css( 'background', 'url(' + keyboardIcon + ') no-repeat left 3px center rgba(255,255,255,0.75) ');
 	$( 'textarea, input[type=text]' ).ime();
-	$( 'input[type=text]' ).ime();
 });
