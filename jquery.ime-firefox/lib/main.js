acad5c0d3e4aa0d11ad2247c700140801a94c903
Prageck <prageck@gmail.com>
2013-05-12 16:36:56 +0530

    Fixed image loading issues in firefox extension

--- jquery.ime-firefox/lib/main.js
+++ jquery.ime-firefox/lib/main.js
@@ -1,9 +1,20 @@
 var data = require("sdk/self").data;
 var pageMod = require("sdk/page-mod");
-var tabs = require("sdk/tabs");
 
 pageMod.PageMod({
 	include: "*",
+	contentStyleFile: data.url("css/jquery.ime.css"),
+	
+	// contentStyle is built dynamically here to include absolute URLs
+	// for the images referenced by jquery.ime.css.
+	// This workaround is needed because we can't use relative URLs 
+	// in contentStyleFile or contentStyle.
+	contentStyle: [
+		".imeselector { background-image: url(" + data.url("images/ime-active.png") + ")}",
+		".imeselector-menu .checked { background-image: url(" + data.url("images/tick.png") + ")}"
+	],
+
+	// Inject the required scripts into the page
 	contentScriptFile: [ data.url("js/jquery.js"),
 							data.url("js/jquery.ime.js"),
 							data.url("js/jquery.ime.selector.js"),
@@ -11,7 +22,8 @@ pageMod.PageMod({
 							data.url("js/jquery.ime.inputmethods.js"),
 							data.url("js/invoke.jquery.ime.js"),
 							data.url("rules/hi/hi-transliteration.js"),
-							data.url("rules/gu/gu-transliteration.js")
-					   ],
-	contentStyleFile: data.url("css/jquery.ime.css")
+							data.url("rules/gu/gu-transliteration.js"),
+							data.url("rules/de/de.js"),
+							data.url("rules/ur/ur-transliteration.js")
+	]
 });
